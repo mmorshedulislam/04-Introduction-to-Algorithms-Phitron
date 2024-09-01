a274d42176f4a91ffbbef72db0ce86ded6033b75
Morshed-Office <morshed952640@gmail.com>
2024-09-01 17:06:04 +0600

    M-01 practice

---
+++ Week - 01 - Basic Graph, BFS & DFS/01 - Basics of Graph and Represntations/practice.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, e;
+    cin >> n >> e;
+    vector<int> mat[n];
+    vector<pair<int, int>> v;
+
+    while (e--)
+    {
+        int a, b;
+        cin >> a >> b;
+        v.push_back({a, b});
+    }
+
+    for (pair<int, int> x : v)
+    {
+        cout << x.first << " " << x.second << endl;
+    }
+
+    return 0;
+}
