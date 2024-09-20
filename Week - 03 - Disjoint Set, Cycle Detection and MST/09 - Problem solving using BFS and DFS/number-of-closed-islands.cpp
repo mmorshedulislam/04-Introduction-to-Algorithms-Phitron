a0f83c2bb0139de6df092336e2245c74625a2efd
Morshed-Office <morshed952640@gmail.com>
2024-09-20 12:09:37 +0600

    M-09 completed

--- Week - 03 - Disjoint Set, Cycle Detection and MST/09 - Problem solving using BFS and DFS/number-of-closed-islands.cpp
+++ Week - 03 - Disjoint Set, Cycle Detection and MST/09 - Problem solving using BFS and DFS/number-of-closed-islands.cpp
@@ -2,25 +2,31 @@ class Solution
 {
 public:
     int n, m;
-    bool vis[305][305];
-    vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+    bool vis[105][105];
     bool isValid(int i, int j) { return i >= 0 && i < n && j >= 0 && j < m; }
-    void dfs(int si, int sj, vector<vector<char>> &grid)
+    vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+    bool flag = false;
+
+    void dfs(int si, int sj, vector<vector<int>> &grid)
     {
         vis[si][sj] = true;
-        for (int i = 0; i < d.size(); i++)
+
+        if (si == 0 || si == n - 1 || sj == 0 || sj == m - 1)
+            flag = false;
+
+        for (int i = 0; i < 4; i++)
         {
             int ci = si + d[i].first;
             int cj = sj + d[i].second;
 
-            if (isValid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == '1')
+            if (isValid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == 0)
             {
                 dfs(ci, cj, grid);
             }
         }
-    };
+    }
 
-    int numIslands(vector<vector<char>> &grid)
+    int closedIsland(vector<vector<int>> &grid)
     {
         memset(vis, false, sizeof(vis));
         n = grid.size();
@@ -31,13 +37,16 @@ public:
         {
             for (int j = 0; j < m; j++)
             {
-                if (!vis[i][j] && grid[i][j] == '1')
+                if (!vis[i][j] && grid[i][j] == 0)
                 {
+                    flag = true;
                     dfs(i, j, grid);
-                    ans++;
+                    if (flag)
+                        ans++;
                 }
             }
         }
+
         return ans;
     }
 };
